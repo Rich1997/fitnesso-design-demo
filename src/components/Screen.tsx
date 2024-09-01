36a370d0550358d2e40e298474845c769e1d1251
Rich1997 <rudronil1997@gmail.com>
2024-09-01 10:49:02 +0530

    Remove screen padding on smaller screens

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -2,7 +2,7 @@ import Layout from "./Layout";
 
 const Screen = () => {
     return (
-        <div className="relative flex min-h-patch flex-col p-8 bg-gradient-to-r from-teal-400 to-yellow-200 items-center justify-center">
+        <div className="relative flex min-h-patch flex-col sm:p-8 p-0 bg-gradient-to-r from-teal-400 to-yellow-200 items-center justify-center">
             <div className="h-dvh sm:w-[412px] w-screen bg-background rounded-xl overflow-clip">
                 <Layout />
             </div>
