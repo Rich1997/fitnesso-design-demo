18a5d44382c927df0dd29abfe94af83602c7327e
Rich1997 <rudronil1997@gmail.com>
2024-09-01 11:22:48 +0530

    Adjust height on desktop

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -3,7 +3,7 @@ import Layout from "./Layout";
 const Screen = () => {
     return (
         <div className="relative flex min-h-patch flex-col sm:p-8 p-0 bg-gradient-to-r from-teal-400 to-yellow-200 items-center justify-center">
-            <div className="h-dvh sm:w-[412px] w-screen bg-background rounded-xl overflow-clip">
+            <div className="sm:h-[866px] h-dvh sm:w-[412px] w-screen bg-background rounded-xl overflow-clip">
                 <Layout />
             </div>
         </div>
