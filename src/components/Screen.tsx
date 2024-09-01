45aeb9a5a5dbb5229d9c7b274f044025bfe172e8
Rich1997 <rudronil1997@gmail.com>
2024-09-01 10:44:44 +0530

    Fix screen height & width

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -3,7 +3,7 @@ import Layout from "./Layout";
 const Screen = () => {
     return (
         <div className="relative flex min-h-patch flex-col p-8 bg-gradient-to-r from-teal-400 to-yellow-200 items-center justify-center">
-            <div className="h-[866px] w-[412px] bg-background rounded-xl overflow-clip">
+            <div className="h-dvh sm:w-[412px] w-screen bg-background rounded-xl overflow-clip">
                 <Layout />
             </div>
         </div>
